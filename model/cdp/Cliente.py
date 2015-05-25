20e5b98c3caa21b41f777a064169fb10013d91f8
Ramna Kun <phoenixproject.erp@gmail.com>
2015-05-24 22:31:56 -0300

    reConstrução dos atributos das classes 3

--- model/cdp/Cliente.py
+++ model/cdp/Cliente.py
@@ -3,4 +3,5 @@ from model.cdp.AbstrcPessoa import Pessoa
 __author__ = 'Gustavo'
 class Cliente(Pessoa):
     def __init__(self):
+        super(Cliente, self).__init__()
         self.email = ""
--- model/cdp/Motorista.py
+++ model/cdp/Motorista.py
@@ -4,4 +4,5 @@ __author__ = 'Gustavo'
 
 class Motorista(Pessoa):
     def __init__(self):
+        super(Motorista, self).__init__()
         self.cnh = ""
